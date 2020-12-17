cc8f6d5cb1f5a7d5877c52fc462a79f47427099f
Saba Qaiser <40913711+whensabacodes@users.noreply.github.com>
2020-12-17 14:18:37 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,19 @@
+var bluerect,redrect;
+
+
+function setup() {
+  createCanvas(800,400);
+  // createSprite(400, 200, 50, 50);
+  bluerect=createSprite(200,200,50,100);
+  bluerect.shapeColor="blue";
+
+}
+
+function draw() {
+  background("lightgreen");  
+  redrect.x=World.mouseX;
+  redrect.y=World.mouseY;
+
+  if(redrect.)
+  drawSprites();
+}
